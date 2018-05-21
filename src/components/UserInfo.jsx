4b75a943ba2daf345827ae0cda8eab543d2126eb
Tati Sattin <tatianes.garcez.oliveira@gmail.com>
2018-05-21 07:20:23 -0300

    Create UserInfo component that handles user data from JSON file

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+export default class UserInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.user,
+      behavior: "default",
+      flag: ""
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.behavior) {
+      this.setState({ behavior: this.props.behavior });
+    }
+
+    if (this.props.flag) {
+      this.setState({ flag: this.props.flag });
+    }
+  }
+
+  getDataFrom(data) {
+    if (data) {
+      return data.map(item => <li>{item.name}</li>);
+    }
+  }
+
+  render() {
+    if (this.state.behavior === "small") {
+      return <h2>{this.state.user.name}</h2>;
+    } else if (this.state.behavior === "picture-only") {
+      return (
+        <img alt={this.state.user.name} src={this.state.user.picture_url} />
+      );
+    }
+
+    const componentElements = {
+      teams: <ul>{this.getDataFrom(this.state.user.teams)}</ul>,
+      tags: <ul>{this.getDataFrom(this.state.user.tags)}</ul>,
+      email: <a href="mailto:{this.user.email}">{this.state.user.email}</a>
+    };
+
+    let info;
+    if (this.state.flag && this.state.user[this.state.flag]) {
+      info = componentElements[this.state.flag];
+    } else {
+      if (this.state.user.teams) {
+        info = componentElements.teams;
+      } else if (this.state.user.tags) {
+        info = componentElements.tags;
+      } else {
+        info = componentElements.email;
+      }
+    }
+
+    return (
+      <div>
+        <img
+          width="50px"
+          alt={this.state.user.name}
+          src={this.state.user.picture_url}
+        />
+
+        <h2>{this.state.user.name}</h2>
+        <small>{this.state.user.custom_message}</small>
+
+        {info}
+      </div>
+    );
+  }
+}
